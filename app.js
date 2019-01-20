abb967d4887b1017e3047291c8a2dcee5bbdd7aa
Tara Fenton <tarafenton@Taras-MBP-3.fios-router.home>
2019-01-19 22:16:15 -0500

    add state

---
+++ app.js
@@ -0,0 +1,20 @@
+state = {
+	recipes: [
+		{
+			id: 10,
+			name: 'Omelette'
+		}
+	],
+	ingredients: [
+		{
+			recipe_id: 10,
+			name: 'Egg',
+			quanity: 2
+		},
+		{
+			recipe_id: 10,
+			name: 'Milk',
+			quanity: 1
+		}
+	]
+}
