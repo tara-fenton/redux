7e887293b9e982794548f06a1ab79a481234ac71
Tara Fenton <tarafenton@Taras-MBP-3.fios-router.home>
2019-01-19 22:22:00 -0500

    store setup

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
-state = {
-	recipes: [
-		{
-			id: 10,
-			name: 'Omelette'
-		}
-	],
-	ingredients: [
-		{
-			recipe_id: 10,
-			name: 'Egg',
-			quanity: 2
-		},
-		{
-			recipe_id: 10,
-			name: 'Milk',
-			quanity: 1
-		}
-	]
-}
+import { createStore } from "redux";
+
+const initialState = {
+  recipes: [
+    {
+      id: 10,
+      name: "Omelette"
+    }
+  ],
+  ingredients: [
+    {
+      recipe_id: 10,
+      name: "Egg",
+      quanity: 2
+    },
+    {
+      recipe_id: 10,
+      name: "Milk",
+      quanity: 1
+    }
+  ]
+};
+
+const reducer = (state, action) => state;
+const store = createStore(reducer, initialState);
+
+window.store = store;
